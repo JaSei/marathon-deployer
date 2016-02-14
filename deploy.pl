5537c015eeafe636d18cbd20ec2ffe2f5d92e644
alenkacz <varkockova.a@gmail.com>
2016-02-14 17:26:25 +0100

    Verify that the app has been deployed
    check /deployments endpoint to see whether an app has been already
    deployed

--- deploy.pl
+++ deploy.pl
@@ -12,7 +12,8 @@ use URI::Escape qw(uri_escape);
 my $marathon_url = $ENV{MARATHON_URL}
     or die 'Environment variable MARATHON_URL not set. Exiting...';
 
-my $marathon_apps_url = "$marathon_url/v2/apps";
+my $marathon_api_url = "$marathon_url/v2";
+my $marathon_apps_url = "$marathon_api_url/apps";
 
 my $marathon_json_file = $ENV{MARATHON_JSON} || 'marathon.json';
 die "The file $marathon_json_file does not exist." unless -e $marathon_json_file;
@@ -36,15 +37,33 @@ if (defined $ENV{MARATHON_INSTANCES}) {
 
 my $ua = Mojo::UserAgent->new;
 
+my $application_id = $marathon_json->{id};
+
 my $app_url = URI->new("$marathon_apps_url/".uri_escape($marathon_json->{id}))->canonical->as_string();
+
 my $res = $ua->put($app_url => json => $marathon_json)->res();
 
 if ($res->code != 200 && $res->code != 201) {
     die $res->to_string();
 }
 
+my $deployment_url = URI->new("$marathon_api_url/deployments")->canonical->as_string();
+
+while (!is_deploy_finished($ua, $deployment_url, $application_id)) {
+	print "Waiting for application to be deployed...";
+	sleep(5);
+}
 
 sub is_nonnegative_integer {
     local $_ = shift;
     return /\A\+?\d+\z/
+}
+
+sub is_deploy_finished {
+	my ($ua, $deployment_url, $application_id) = @_;
+	my $res = $ua->get($deployment_url)->res();
+	my $parsed_result = decode_json($res->body);
+	my $applications_being_deployed = $parsed_result->[0]->{affectedApps};
+	
+	return not ("/$application_id" ~~ $applications_being_deployed);
 }
