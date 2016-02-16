19d715a145511bd3973dc86433f6452b1ad3a663
alenkacz <varkockova.a@gmail.com>
2016-02-16 11:22:47 +0100

    Show number of ongoing deployments

--- deploy.pl
+++ deploy.pl
@@ -49,9 +49,11 @@ if ($res->code != 200 && $res->code != 201) {
 
 my $deployment_url = URI->new("$marathon_api_url/deployments")->canonical->as_string();
 
-while (!is_deploy_finished($ua, $deployment_url, $application_id)) {
-	print "Waiting for application to be deployed...";
+my $number_of_deployments = number_of_deployments($ua, $deployment_url, $application_id);
+while ($number_of_deployments > 0) {
+	print "Waiting for $number_of_deployments deployment(s) to finish...";
 	sleep(5);
+	$number_of_deployments = number_of_deployments($ua, $deployment_url, $application_id);
 }
 
 sub is_nonnegative_integer {
@@ -59,11 +61,18 @@ sub is_nonnegative_integer {
     return /\A\+?\d+\z/
 }
 
-sub is_deploy_finished {
+sub number_of_deployments {
 	my ($ua, $deployment_url, $application_id) = @_;
 	my $res = $ua->get($deployment_url)->res();
 	my $parsed_result = decode_json($res->body);
-	my $applications_being_deployed = $parsed_result->[0]->{affectedApps};
 	
-	return not ("/$application_id" ~~ $applications_being_deployed);
+	my $active_deployments = 0;
+	foreach my $deployment (@$parsed_result) {
+		my $applications_being_deployed = $deployment->{affectedApps};
+		if (grep {$_ eq "/$application_id"} @$applications_being_deployed) {
+				$active_deployments++;
+		}
+	}
+	
+	return $active_deployments;
 }
